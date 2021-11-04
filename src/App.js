5e838e8468cf8029e03ea6bbcd3ac9e80bfb0ac9
CesarCadena5 <cesarojitos555@hotmail.com>
2021-11-04 11:12:48 -0500

    primera version con css en carpeta SRC

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import { Formik, Form, Field } from 'formik';
+import './header.css';
+import './content.css';
+import './article.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+const App = () => {
+  const [fotos, setFotos] = useState([]);
+  console.log(fotos);
+  const open = (url) => window.open(url);
+  const submit = async ({search}) => {
+    //llamar a API
+    const URL = `https://api.unsplash.com/search/photos?per_page=20&query=${search}`;
+    const response = await fetch(URL, { 
+      headers: {
+        'Authorization' : 'Client-ID 03ZSBI5E4eFPA_NyRe_jHdCMLl8rRYRgwMohLbaaGMI'
+      } 
+    });
+
+    const data = await response.json();
+    setFotos(data.results);
+  }
+
+  return(
+    <div>
+      <header>
+        <Formik
+          initialValues={{search: ''}}
+          onSubmit={submit}
         >
-          Learn React
-        </a>
+          <Form>
+            <Field name='search' />
+          </Form>
+        </Formik>
       </header>
+      <div className="container">
+        <div className="center">
+          {fotos.map(foto => 
+            <article key={foto.id} onClick={() => open(foto.links.html)}>
+              <img src={foto.urls.regular} alt='foto' />
+              <p>{[foto.description, foto.alt_description].join(' - ')}</p>
+            </article>)}
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
